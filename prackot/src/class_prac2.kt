ffa73f3e3bc4e6847cf31136e92496e76ebae621
staris <sungdoolim@naver.com>
2020-05-27 01:17:05 +0900

    data class practice

--- prackot/src/class_prac2.kt
+++ prackot/src/class_prac2.kt
@@ -1,4 +1,4 @@
-class animal(){
+open class animal(){
     constructor(name:String,age:Int):this(){
         this.name=name
         this.age=age
@@ -9,11 +9,34 @@ class animal(){
         println("name:$name , age:$age")
     }
 }
+data class Dog(var dna: Int):animal()  // dna name age 3개의 자료만 담고 있음
+
+object fac {
+    var arl= arrayListOf<Any>()
+    var count=0;
+    fun pregna():Dog{
+        count+=1;
+        var a=Dog(count)
+        arl.add(a)
+        return a
+
+    }
+}
+
+
 
 fun main():Unit{
     var ani=animal()
     var ani2=animal("staris",25)
     ani.pt()
     ani2.pt()
-      
+    var dog=Dog(3)
+    dog.pt(); println(dog.dna)
+
+    var dog1=fac.pregna()
+    dog1.pt();println(dog1.dna)
+    var dog2=fac.pregna()
+    dog2.pt();println(dog2.dna)
+
+
 }
